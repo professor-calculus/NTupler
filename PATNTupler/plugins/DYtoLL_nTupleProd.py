b7399eb03ebdd94d4de7a336a4125cf6a52ba46c
Emmanuel Olaiya <emmanuel.olaiya@stfc.ac.uk>
2015-09-24 10:07:47 +0100

    Added crab configs

---
+++ PATNTupler/plugins/DYtoLL_nTupleProd.py
@@ -0,0 +1,18 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'dy_nTuple_production'
+config.General.workArea = 'crab_projects'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'nTupleProduction.py'
+
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
+config.Data.outLFNDirBase = '/store/user/olaiya/NTuples/DY' #Wherever you want the ntuples to go on the RAL T2
+config.Data.publication = False
+config.Data.publishDataName = 'DY_nTuple_PROD'
+
+config.Site.storageSite = 'T2_UK_SGrid_RALPP'
