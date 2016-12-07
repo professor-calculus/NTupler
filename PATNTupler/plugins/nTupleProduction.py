8180b2002f6f5eca73a04e73edad6b60016ed8a3
Emmanuel Olaiya <emmanuel.olaiya@stfc.ac.uk>
2016-12-07 16:09:59 +0000

    Updated config file to work for MC Ntuple production

--- PATNTupler/plugins/nTupleProduction.py
+++ PATNTupler/plugins/nTupleProduction.py
@@ -1,69 +1,84 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("Demo")
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing ('analysis')
+options.register ('useMiniAOD',
+                  False,
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.bool,
+                  "use miniAOD rather than AOD")
+options.parseArguments()
+useMiniAOD=options.useMiniAOD
 
+# set up process
+process = cms.Process("HEEP")
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
-
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-        #'root://dcap.pp.rl.ac.uk//pnfs/pp.rl.ac.uk/data/cms/store/mc/Spring14miniaod/TTbarH_M-125_13TeV_amcatnlo-pythia8-tauola/MINIAODSIM/PU20bx25_POSTLS170_V5-v1/00000/7CB96282-EA1E-E411-84B7-001E67396A22.root'
-        #'file:/opt/ppd/scratch/olaiya/CMS/analysis/CMSSW_7_2_0/src/PhysicsTools/PatAlgos/test/miniAOD/patTuple.root'
-#lcg-ls "srm://heplnx204.pp.rl.ac.uk:8443/srm/managerv2?SFN=/pnfs/pp.rl.ac.uk/data/cms/store/mc/Spring14miniaod/TTbarH_M-125_13TeV_amcatnlo-pythia8-tauola/MINIAODSIM/PU20bx25_POSTLS170_V5-v1/00000/7CB96282-EA1E-E411-84B7-001E67396A22.root"
-        'root://dcap.pp.rl.ac.uk:1094/pnfs/pp.rl.ac.uk/data/cms/store/user/olaiya/PatTuples/MiniAOD/TT/TT_TuneCUETP8M1_13TeV-powheg-pythia8/TT_MiniAOD_PROD/150825_203915/0000/patTuple_MC_43.root'
-        #'/store/data/Run2015B/DoubleEG/MINIAOD/PromptReco-v1/000/251/244/00000/6A0A8868-4B27-E511-B3F8-02163E011BD1.root',
-    )
+process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
+    reportEvery = cms.untracked.int32(10),
+    limit = cms.untracked.int32(1500)
 )
-
-#heep event needs geom & b-field
-process.load("Configuration.Geometry.GeometryRecoDB_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-process.GlobalTag.globaltag = cms.string('MCRUN2_74_V9A')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
 
 process.TFileService = cms.Service("TFileService", fileName=cms.string('nTuple.root'))
 
-#from SHarper.HEEPAnalyzer.HEEPSelectionCuts_cfi import *
+#setup global tag
+from Configuration.AlCa.GlobalTag import GlobalTag
+from Configuration.AlCa.autoCond import autoCond
+process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_miniAODv2_v1', '') #
 
-process.demo = cms.EDAnalyzer("RALMiniAnalyzer",
-    isThisMC = cms.bool(True),
-    heepId = cms.InputTag("heepId"),
-    vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
-    muons = cms.InputTag("slimmedMuons"),
-    electrons = cms.InputTag("slimmedElectrons"),
-    jets = cms.InputTag("slimmedJets"),
-    fatjets = cms.InputTag("slimmedJetsAK8"),
-    mets = cms.InputTag("slimmedMETs"),       
-    bits = cms.InputTag("TriggerResults","","HLT"),
-    prescales = cms.InputTag("patTrigger"),
-    objects = cms.InputTag("selectedPatTrigger"),
-    selectedTriggerPaths = cms.vstring("HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW_v","HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v","HLT_Mu30_Ele30_CaloIdL_GsfTrkIdVL_v")#matches triggers that *contain* the stated names, so finish with _v to make sure your trigger name isn't a subset of others
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(2000) )
+process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(
+        'root://dcap.pp.rl.ac.uk:1094/pnfs/pp.rl.ac.uk/data/cms/store/mc/RunIISpring16MiniAODv1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3_ext1-v1/20000/AC5DA257-68FC-E511-8E8F-5065F381E271.root'
 )
-
-#setting up the producer to make the HEEP ID value map
-from SHarper.HEEPAnalyzer.HEEPSelectionCuts_cfi import *
-process.heepId = cms.EDProducer("HEEPIdValueMapProducer",
-                                eleLabel = cms.InputTag("gedGsfElectrons"),
-                                barrelCuts = cms.PSet(heepBarrelCuts),
-                                endcapCuts = cms.PSet(heepEndcapCuts),
-                                eleIsolEffectiveAreas = cms.PSet(heepEffectiveAreas),
-                                eleRhoCorrLabel = cms.InputTag("fixedGridRhoFastjetAll"),
-                                applyRhoCorrToEleIsol = cms.bool(True),
-                                verticesLabel = cms.InputTag("offlinePrimaryVertices"),
-                                writeIdAsInt =cms.bool(True) #true saves the heep ID as an int, false: saves as a float, user request
-                                )
-   
-process.heepId.barrelCuts.minEt=cms.double(35.)
-process.heepId.endcapCuts.minEt=cms.double(35.)
-
+                                       
+)
 useMiniAOD=True
+
 if useMiniAOD==True:
-    process.heepId.verticesLabel=cms.InputTag("offlineSlimmedPrimaryVertices")
-    process.heepId.eleLabel=cms.InputTag("slimmedElectrons")
+#    process.source.fileNames=cms.untracked.vstring('root://dcap.pp.rl.ac.uk:1094/pnfs/pp.rl.ac.uk/data/cms/store/mc/RunIISpring16MiniAODv1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3_ext1-v1/20000/AC5DA257-68FC-E511-8E8F-5065F381E271.root',)
+    process.source.fileNames=cms.untracked.vstring('/store/mc/RunIISpring16MiniAODv1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3_ext1-v1/20000/AC5DA257-68FC-E511-8E8F-5065F381E271.root',)
+
+#setup the VID with HEEP 7.0
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+# turn on VID producer, indicate data format  to be
+# DataFormat.AOD or DataFormat.MiniAOD, as appropriate
+if useMiniAOD:
+    switchOnVIDElectronIdProducer(process, DataFormat.MiniAOD)
+else:
+    switchOnVIDElectronIdProducer(process, DataFormat.AOD)
+
+# define which IDs we want to produce
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV70_cff']
+#add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
 
 
-#process.load("SHarper.HEEPAnalyzer.HEEPAnalyzer_cfi")
-#from SHarper.HEEPAnalyzer.HEEPAnalyzer_cfi import swapHEEPToMiniAOD
-#swapHEEPToMiniAOD(process.heepAnalyzer)
+#this is our example analysis module reading the results
+process.demo = cms.EDAnalyzer("RALMiniAnalyzer",
+                                       isThisMC = cms.bool(False),
+                                       #mcWeight = cms.double(MCWEIGHT_INSERTEDHERE),
+                                       heepId = cms.InputTag("heepId"),
+                                       vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                                       muons = cms.InputTag("slimmedMuons"),
+                                       electrons = cms.InputTag("slimmedElectrons"),
+                                       jets = cms.InputTag("slimmedJets"),
+                                       fatjets = cms.InputTag("slimmedJetsAK8"),
+                                       mets = cms.InputTag("slimmedMETs"),       
+                                       bits = cms.InputTag("TriggerResults","","HLT"),
+                                       prescales = cms.InputTag("patTrigger"),
+                                       objects = cms.InputTag("selectedPatTrigger"),
+                                       selectedTriggerPaths = cms.vstring("HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW_v","HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v","HLT_Mu30_Ele30_CaloIdL_GsfTrkIdVL_v"),#matches triggers that *contain* the stated names, so finish with _v to make sure your trigger name isn't a subset of others
+                                       elesAOD=cms.InputTag("gedGsfElectrons"),
+                                       elesMiniAOD=cms.InputTag("slimmedElectrons"),
+                                       trkIsolMap=cms.InputTag("heepIDVarValueMaps","eleTrkPtIso"),
+                                       vid=cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV70")
+                                       )
 
-process.p = cms.Path(process.heepId*process.demo)
+process.p = cms.Path(
+    process.egmGsfElectronIDSequence* 
+    process.demo) #our analysing example module, replace with your module
+
