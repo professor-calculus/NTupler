a8d4d4d7dab34f101fd1c7daffc462246bcdf536
Emmanuel Olaiya <emmanuel.olaiya@stfc.ac.uk>
2016-12-13 12:46:55 +0000

    Example selection for running over ntuple

--- PATNTupler/main/main.cc
+++ PATNTupler/main/main.cc
@@ -95,68 +95,20 @@ int main(int argc, char** argv){
   //Example of two ways to access the data.
   //1. Using helper class NtpReader
   //2. Standard ROOT DIY
-
-  //1. Method 1. Using NtpReader
-  ///*
-  //loop over files
-  /*
-  for (std::vector<string>::const_iterator iter = vectorOfFiles.begin();
-       iter != vectorOfFiles.end(); ++iter){//loop over ntuple files
-    unique_ptr<TFile> g = unique_ptr<TFile> (new TFile((*iter).c_str() ));
-
-    TTree *eventtTree = (TTree*)g->Get("demo/EventDataTree"); //get the tree
-
-    NtpReader testE(eventtTree, "evtInfo"); //instantiate NtpReader which takes tree to analyse as input
-    //testE.setEventInfoBranch("evtInfo");//Specify the branches you want to look at. Each branch has its own setter method. Need to move this to the constructor
-    cout << "Num of Entries is: " <<  testE.getLastEntry() << "\n";
-
     
-    for (testE.setEntryNumber(0); testE.getEntryNumber() < testE.getLastEntry(); testE.nextEntry()){//loop over events in a file
       
-      cout << "Entry number is: " << testE.getEntryNumber() << "\n";
-      cout <<"Run Number is: " << testE.runNum() << "\n";
-      cout <<"Event Number is: " << testE.evtNum() << "\n";
-
-      //let's test the new method
-
-      std::vector<ran::NtElectron> elist = testE.getParticleCollection<ran::NtElectron, ran::ElectronStruct>("electronCollection");
-    
-      if (!elist.empty()){
-	cout << "New member EventNumber: " << testE.getEntryNumber() << "\n";
-	cout << "electron Pt: " << (elist.at(0)).pt() << "\n";     //print pt
-	//cout << "HEEP et: " << (elist.at(0)).heep_et() << "\n";
-      }
-    
-      std::vector<ran::NtJet> jlist = testE.getParticleCollection<ran::NtJet, ran::JetStruct>("jetCollection");
-      if (!jlist.empty()){
-	cout << "Jet Et: " << (jlist.at(0)).et() << "\n"; //print et
-      }
-    
-      std::vector<ran::NtMuon> mlist = testE.getParticleCollection<ran::NtMuon, ran::MuonStruct>("muonCollection");
-      if (!mlist.empty()){
-	cout << "Muon pt: " << (mlist.at(0)).pt() << "\n"; //print et
-      }
-      
-      
-      //at the end of the loop
-      //testE.nextEntry();//increment eventnumber in ntuple
-    }//end of loop over number of events in a file
-  }//end of loop over ntuple files
-  //
-  */
-
   //2. Method 2. DIY ROOT
   TFile fout(outRootFile,"recreate");
   //TFile f("out.root","new");
-  GoodLumiChecker glc(jsonFile);//object to check if event passed certification
-  unique_ptr<TH1F> eeMass(new TH1F("eeMass","ee Mass",400,0,2000)); 
-  eeMass->SetDirectory(&fout);
+  //GoodLumiChecker glc(jsonFile);//object to check if event passed certification
+  TH1F mumuMass("mumuMass","mu mu Mass",100,0,200); 
+  mumuMass.SetDirectory(&fout);
   for (std::vector<string>::const_iterator fIter = vectorOfFiles.begin();
        fIter != vectorOfFiles.end(); ++fIter){//loop over ntuple files
     unique_ptr<TFile> f = unique_ptr<TFile> (new TFile((*fIter).c_str() ));
     std::cout << "Opening file: " << *fIter << "\n";
     TTree *evtTree = (TTree*)f->Get("demo/EventDataTree"); //get the tree
-    TTree *trigTree = (TTree*)f->Get("demo/TriggerPathsTree"); //get the tree
+    //TTree *trigTree = (TTree*)f->Get("demo/TriggerPathsTree"); //get the tree
     //We need to set up a pointer to  a vector of particle pointers (forgot what i originally meant here!)
     //using smart pointers so looks messy. Won't bother using typedefs to reduce the mess
     unique_ptr<ran::EventInfo> evtObj(new ran::EventInfo());
@@ -181,112 +133,97 @@ int main(int argc, char** argv){
     TBranch* jetBranch = evtTree->GetBranch("jetCollection"); //load jet collection
     jetBranch->SetAddress(&jetVector);
 
-    TBranch* fatjetBranch = evtTree->GetBranch("fatjetCollection"); //load jet collection
-    fatjetBranch->SetAddress(&fatjetVector);
+    //TBranch* fatjetBranch = evtTree->GetBranch("fatjetCollection"); //load jet collection
+    //fatjetBranch->SetAddress(&fatjetVector);
 
-    TBranch* metBranch = evtTree->GetBranch("metCollection"); //load met collection
-    metBranch->SetAddress(&metVector);
+    //TBranch* metBranch = evtTree->GetBranch("metCollection"); //load met collection
+    //metBranch->SetAddress(&metVector);
+    
+    //TBranch* trigFlagBranch = evtTree->GetBranch("recordedTriggers"); //load met collection
+    //trigFlagBranch->SetAddress(&triggerFlagsVector);
 
-    TBranch* trigFlagBranch = evtTree->GetBranch("recordedTriggers"); //load met collection
-    trigFlagBranch->SetAddress(&triggerFlagsVector);
-
-    TBranch* trigBranch = trigTree->GetBranch("triggerPaths"); //load trigger collection
-    trigBranch->SetAddress(&triggerPathVector);
-
-    const unsigned int trigEntryNum = trigBranch->GetEntries()-1;
-
-     //Which trigger bit corresponds to trigger path HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW_v
-    TriggerPathToIndex tpti(*triggerPathVector); //Load in the list of trigger paths recoreded
-    unsigned int trigIndex = tpti.getTrigIndex("HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW_v");
-
+    //TBranch* trigBranch = trigTree->GetBranch("triggerPaths"); //load trigger collection
+    //trigBranch->SetAddress(&triggerPathVector);
+    
+    //const unsigned int trigEntryNum = trigBranch->GetEntries()-1;
+    
     const unsigned int numEvents = branch->GetEntries(); //All evtTree branches *should* have the same number of entries
 
     std::cout << "Number of Entries is: " << numEvents << "\n";
-
+    
     //loop over tree
     for (unsigned int i = 0; i < numEvents; ++i){
       branch->GetEntry(i); // set tree object for each event i
       evtBranch->GetEntry(i); // set tree object for each event i
-      //Electron collection contain structs (ElectronStruct). We want to use our own electon class which is composed of the struct (NtElectron) 
-
-      if (isMC || glc.isGoodLumiSec(evtObj->runNum,evtObj->lumiSec)){
-
-	unique_ptr<std::vector<ran::NtElectron> > ralEVector(new std::vector<ran::NtElectron>(electronVector->begin(), electronVector->end()));
-	//This function generates a vector of NtElectron from a vector of ElectronStruct
-	//fillE(ralEVector.get(),electronVector.get());
-	//fillParticleVector<ran::NtElectron, ran::ElectronStruct>(ralEVector.get(),electronVector.get());
-
-	std::vector<std::vector<ran::NtElectron>::const_iterator> heepElectrons;
-	for (std::vector<ran::NtElectron>::const_iterator iter = ralEVector->begin();
-	     iter != ralEVector->end(); ++iter){ //look at the electrons using our electron class
-
-	  //Store electrons that pass HEEP cutcodes
-	  if ( !(iter->heep_cutCode()) ){
-	    if (iter-> heep_et() >35.0){
-	    
-	      trigFlagBranch->GetEntry(i);
-	      //std::cout << "First Trigger bit is: " << int(triggerFlagsVector->at(0)) << "\n";
-	      trigBranch->GetEntry(trigEntryNum);//Entries should be the same
-	      //std::cout << "First Trigger is: " << triggerPathVector->at(0) << "\n";	   
-	      //if (int(triggerFlagsVector->at(trigIndex))){
-	      heepElectrons.push_back(iter);
-	      //}
-	    }
-	  }     
-	}
-
-	//do we have at least two HEEP electrons    
-	if (heepElectrons.size() > 1){
-	  //OK order them by pt. Highest first      
-	  //std::sort(heepElectrons.begin(), heepElectrons.end(), compareLeptonPt{}); //Think they are already sorted
-	  TLorentzVector ele1, ele2, res4v; 
-	  ele1.SetPtEtaPhiM( heepElectrons[0]->pt() ,  heepElectrons[0]-> heep_eta(),  heepElectrons[0]-> heep_phi(), 0.000511);
-	  ele2.SetPtEtaPhiM( heepElectrons[1]->pt() ,  heepElectrons[1]-> heep_eta(),  heepElectrons[1]-> heep_phi(), 0.000511);
-	  res4v = ele1+ ele2;
-	  double mass = res4v.M();
-	  eeMass->Fill(mass);
-	}
+      
 
 	//can do the same with muons and jets
-    
+        //Select Muons
 	muonBranch->GetEntry(i); // set tree object eMeEvent for each event i
 
-	unique_ptr<std::vector<ran::NtMuon> > ralMuVector(new std::vector<ran::NtMuon>(muonVector->begin(), muonVector->end()));
-	if(!ralMuVector->empty()){//if it is not empty
-	  //std::cout << "muon size: " << ralMuVector->size() << "\n";
-	  //std::cout << "muon pt: "<< (ralMuVector->at(0)).pt() << "\n";
-	}
+	//unique_ptr<std::vector<ran::NtMuon> > ralMuVector(new std::vector<ran::NtMuon>(muonVector->begin(), muonVector->end()));
+        std::vector<ran::NtMuon> ralMuVector(muonVector->begin(), muonVector->end());
+
+	std::vector<const ran::NtMuon*> selectedMuonsPos;
+	std::vector<const ran::NtMuon*> selectedMuonsNeg;
+	if(!ralMuVector.empty()){//if it is not empty
+
+	  for (const ran::NtMuon& muon : ralMuVector){
+	    if (muon.tuneP_exists()){
+	      if (muon.tuneP_pt() > 25.0){
+		if (fabs(muon.tuneP_eta()) < 2.1){
+		  if (muon.tuneP_charge() > 0){
+		    selectedMuonsPos.push_back(&muon);
+		  } else {
+		    selectedMuonsNeg.push_back(&muon);
+		  }
+		}//eta cut
+	      }//pt cut
+	    }//tune P muons
+	    //std::cout << "muon size: " << ralMuVector->size() << "\n";
+	    //std::cout << "muon pt: "<< (ralMuVector->at(0)).pt() << "\n";
+	  }
+	}	
 
 	jetBranch->GetEntry(i); // set tree object for each event i
-
-	unique_ptr<std::vector<ran::NtJet> > ralJetVector(new std::vector<ran::NtJet>(jetVector->begin(), jetVector->end()));
-	if(!ralJetVector->empty()){//if it is not empty
-	  //std::cout << "jet size: " << ralJetVector->size() << "\n";
-	  //std::cout << "jet pt: "<< (ralJetVector->at(0)).pt() << "\n";
+ 
+	std::vector<const ran::NtJet*> bCandJets;
+	std::vector<const ran::NtJet*> forwardJets;
+	std::vector<ran::NtJet>  ralJetVector(jetVector->begin(), jetVector->end());
+	for (const ran::NtJet& jet : ralJetVector){
+	  if (jet.pt()> 30.0){
+	    if (fabs(jet.eta()) < 2.4){
+	      bCandJets.push_back(&jet);
+	    }
+	    if ((fabs(jet.eta()) > 2.4) && (fabs(jet.eta()) < 4.7)){
+	      forwardJets.push_back(&jet);
+	    }
+	  }
 	}
 
-	fatjetBranch->GetEntry(i); // set tree object for each event i
-
-	unique_ptr<std::vector<ran::NtFatJet> > ralfatJetVector(new std::vector<ran::NtFatJet>(fatjetVector->begin(), fatjetVector->end()));
-	if(!ralfatJetVector->empty()){//if it is not empty
-	  //std::cout << "fat jet size: " << ralfatJetVector->size() << "\n";
-	  //std::cout << "fat jet pt: "<< (ralfatJetVector->at(0)).pt() << "\n";
-	}
-
-	metBranch->GetEntry(i); // set tree object for each event i
-
-	unique_ptr<std::vector<ran::NtMet> > ralMetVector(new std::vector<ran::NtMet>(metVector->begin(), metVector->end()));
-	if(!ralMetVector->empty()){//if it is not empty
-	  //std::cout << "met size: " << ralMetVector->size() << "\n";
-	  //std::cout << "met pt: "<< (ralMetVector->at(0)).pt() << "\n";
-	}      
-
-      }// Certified event?
-    }//loop over events
+	if (!forwardJets.empty()){
+	  if (bCandJets.size() == 1){
+	    if (bCandJets[0]->pfCombinedMVAV2BJetTags() > -0.715){
+	      if (!selectedMuonsPos.empty()){
+		if (!selectedMuonsNeg.empty()){
+		  TLorentzVector mu1, mu2, res4v; 
+		  mu1.SetPtEtaPhiM( selectedMuonsPos[0]->tuneP_pt() ,  selectedMuonsPos[0]->tuneP_eta(),  selectedMuonsPos[0]->tuneP_phi(), 0.105658);
+		  mu2.SetPtEtaPhiM( selectedMuonsNeg[0]->tuneP_pt() ,  selectedMuonsNeg[0]->tuneP_eta(),  selectedMuonsNeg[0]->tuneP_phi(), 0.105658);
+		  res4v = mu1+ mu2;
+		  double mass = res4v.M();
+		  mumuMass.Fill(mass);
+		}// at least one negative muon	  
+	      } // at least one postive muon
+	    }//
+	  }//seleted jets
+	}//At least one forward jet       	
+	//}// Certified event?
+    }//loop over events 
+    std::cout << "Closing file: " << *fIter << "\n";
   }//loop over files
 
   fout.cd();
-  eeMass->Write();
+  mumuMass.Write();
   fout.Write();
 
   //graphicsPlease.Run();
