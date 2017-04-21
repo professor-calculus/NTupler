f2344dfc6db4fc05e9862e7b0a6534e5e9da9f32
joseph-taylor <joseph.ross.taylor@cern.ch>
2017-04-21 13:08:55 +0100

    added batch script to set the jobs off

---
+++ PATNTupler/batchJobs/setoffJobs.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# RUN WITH > source $CMSSW_BASE/src/NTupler/PATNTupler/batchJobs/setoffJobs.sh
+
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/TTJets_HT1200to2500/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/TTJets_HT2500toInf/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/TTJets_HT600to800/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/TTJets_HT800to1200/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/TTJets_inclusiveHt/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/WJetsToQQ_HT600toInf/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/WWTo1L1Nu2Q/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/WWTo4Q/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/ZJetsToQQ_HT600toInf/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/ZZTo2L2Q/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/ZZTo2Q2Nu/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/ZZTo4Q/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH30p0_mSusy1200p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH30p0_mSusy1600p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH30p0_mSusy2000p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH30p0_mSusy800p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH50p0_mSusy1200p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH50p0_mSusy1600p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH50p0_mSusy2000p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH50p0_mSusy800p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH70p0_mSusy1200p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH70p0_mSusy1600p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH70p0_mSusy2000p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH70p0_mSusy800p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH90p0_mSusy1200p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH90p0_mSusy1600p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH90p0_mSusy2000p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
+cd /opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_04_22/mH90p0_mSusy800p0_ratio0p99_splitting0p1/tmp/
+./submitCondorJobs_nTupAnaNMSSM.sh
