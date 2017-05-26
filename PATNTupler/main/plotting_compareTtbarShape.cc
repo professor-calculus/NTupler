748bc79588765818cb7f350f6c29f0f9cb1b07c2
joseph-taylor <joseph.ross.taylor@cern.ch>
2017-05-26 17:05:15 +0100

    now works out save name and legend names from given inputs

--- PATNTupler/main/plotting_compareTtbarShape.cc
+++ PATNTupler/main/plotting_compareTtbarShape.cc
@@ -33,12 +33,11 @@ int main(){
 
     // ONE: save info
     std::string outputDir = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/output_ttbarShapeStudies/"; // where we are going to save the output plots
-    std::string outputFile = "varyHT.pdf";
 
     // TWO: set of cut params for plot
     std::vector<std::vector<std::string>> cut2_ak8Dbt = { {"Tight","Med"} };// "Loose", "Med", or "Tight" (2 elements in sub-vector, 1st for leading DBT)
-    std::vector<int> cut3_ak8Pt = {300};
-    std::vector<std::vector<int>>cut4_ht = { {1500,2500}, {2500,3500}, {3500,99000} }; // these are HT bins, not just cuts
+    std::vector<int> cut3_ak8Pt = {300, 400};
+    std::vector<std::vector<int>> cut4_ht = { {2500,3500}, {3500,13000} }; // these are HT bins, not just cuts
     std::vector<std::vector<int>> cut5_ak4Pt = { {250,250} }; // (2 elements in sub-vector, 1st for leading pt)
 
     // THREE: plot settings
@@ -55,7 +54,6 @@ int main(){
     std::string dirExistCommand = "test -e " + outputDir;
     std::string makeDirCommand = "mkdir -p " + outputDir;
     if (std::system(dirExistCommand.c_str()) != 0) std::system(makeDirCommand.c_str());
-    outputFile = outputDir + "/" + outputFile;
 
     std::vector<PlotEntry> setOfTtbar;
     for (size_t iCut2 = 0; iCut2 < cut2_ak8Dbt.size(); ++iCut2){
@@ -65,18 +63,54 @@ int main(){
 
 					std::string cutToApply = Form("fatJetA_doubleBtagDiscrim>%f && fatJetB_doubleBtagDiscrim>%f && fatJetA_p4.Pt()>%d && fatJetB_p4.Pt()>%d && ht>=%d && ht<%d && slimJetA_p4.Pt()>%d && slimJetB_p4.Pt()>%d", DoubleBTagWPs::dbtNameToDouble(cut2_ak8Dbt[iCut2][0]), DoubleBTagWPs::dbtNameToDouble(cut2_ak8Dbt[iCut2][1]), cut3_ak8Pt[iCut3], cut3_ak8Pt[iCut3], cut4_ht[iCut4][0], cut4_ht[iCut4][1], cut5_ak4Pt[iCut5][0], cut5_ak4Pt[iCut5][1]);
 
-					PlotEntry ttbar = PlotEntry("ttbar+jets", hTemplate, varToPlot.c_str(), 50.0); // note that the luminosity value doesn't matter as we will normalise later
+                    std::string legendName = "";
+
+                    if (cut2_ak8Dbt.size()>1) legendName += "DBT " + cut2_ak8Dbt[iCut2][0] + ":" + cut2_ak8Dbt[iCut2][1];
+                    
+                    if (cut3_ak8Pt.size()>1){
+                        if (legendName.size() == 0) legendName += Form("AK8PT %d", cut3_ak8Pt[iCut3]);
+                        else legendName += Form("_AK8PT %d", cut3_ak8Pt[iCut3]);
+                    }
+                    
+                    if (cut4_ht.size()>1){
+                        if (legendName.size() == 0){
+                            if (cut4_ht[iCut4][1]==13000) legendName += Form("HT %d+", cut4_ht[iCut4][0]);
+                            else legendName += Form("HT %d-%d", cut4_ht[iCut4][0], cut4_ht[iCut4][1]);
+                        }
+                        else if (cut4_ht[iCut4][1]==13000) legendName += Form("_HT %d+", cut4_ht[iCut4][0]);
+                        else legendName += Form("_HT %d-%d", cut4_ht[iCut4][0], cut4_ht[iCut4][1]);
+                    }
+                    
+                    if (cut5_ak4Pt.size()>1){
+                        if (legendName.size() == 0) legendName += Form("AK4PT %d:%d", cut5_ak4Pt[iCut5][0], cut5_ak4Pt[iCut5][1]);
+                        else legendName += Form("_AK4PT %d:%d", cut5_ak4Pt[iCut5][0], cut5_ak4Pt[iCut5][1]);
+                    }                    
+
+					PlotEntry ttbar = PlotEntry(legendName.c_str(), hTemplate, varToPlot.c_str(), 50.0); // note that the luminosity value doesn't matter as we will normalise later
 					ttbar.AddInput("/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_05_18/TTJets_inclusiveHt/flatTree.root", Form("%s && lheHT<700.0",cutToApply.c_str()), 831.76);
 					ttbar.AddInput("/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_05_18/TTJets_HT600to800/flatTree.root", Form("%s && lheHT>=700.0",cutToApply.c_str()), 2.6665344485, false);
 					ttbar.AddInput("/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_05_18/TTJets_HT800to1200/flatTree.root", Form("%s && lheHT>=700.0",cutToApply.c_str()), 1.0980821984, false);
 					ttbar.AddInput("/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_05_18/TTJets_HT1200to2500/flatTree.root", Form("%s && lheHT>=700.0",cutToApply.c_str()), 0.1987479092, false);
 					ttbar.AddInput("/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/flatTrees_2017_05_18/TTJets_HT2500toInf/flatTree.root", Form("%s && lheHT>=700.0",cutToApply.c_str()), 0.002368412585, false);
+                    ttbar.NormalisePlot();
 					setOfTtbar.push_back(ttbar);
 
                 } // closes loop through cut 5 states
             } // closes loop through cut 4 states
         } // closes loop through cut 3 states
     } // closes loop through cut 2 states
+
+    std::string saveName = "";
+    if (cut2_ak8Dbt.size()>1) saveName += "dbtVary";
+    else saveName += "dbt" + cut2_ak8Dbt[0][0] + cut2_ak8Dbt[0][1];
+    if (cut3_ak8Pt.size()>1) saveName += "_ak8ptVary";
+    else saveName += Form("_ak8pt%d", cut3_ak8Pt[0]);
+    if (cut4_ht.size()>1) saveName += "_htVary";
+    else saveName += Form("_ht%dto%d",cut4_ht[0][0],cut4_ht[0][1]);
+    if (cut5_ak4Pt.size()>1) saveName += "_ak4ptVary";
+    else saveName += Form("_ak4pt%dn%d", cut5_ak4Pt[0][0], cut5_ak4Pt[0][1]);
+    saveName += ".pdf";
+    std::string outputFile = outputDir + "/" + saveName;
 
 	Plotter plot = Plotter(setOfTtbar);
 	plot.AddLegend(0.6, 0.8, 0.6, 0.8);
