34cb8137476d1a1e667637317d3bf5aba3fc737e
joseph-taylor <joseph.ross.taylor@cern.ch>
2017-06-30 16:13:22 +0100

    there is a comment which when selected puts the numbers on the 2d scatter plot

--- PATNTupler/src/Plotter.cc
+++ PATNTupler/src/Plotter.cc
@@ -331,6 +331,7 @@ void Plotter::Save2D(const std::string& saveName){
 	for (std::vector<PlotEntry2D>::const_iterator iHistos2D = histos2D.begin(); iHistos2D != histos2D.end(); ++iHistos2D){
 		iHistos2D->GetHistogram()->SetEntries(1);
 		iHistos2D->GetHistogram()->Draw("colz, same");
+		// iHistos2D->GetHistogram()->Draw("colz, same, text");
 	}
 
 	if (addLatex) DrawLatex();
