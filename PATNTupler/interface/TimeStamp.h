3c773a511d0a51e6482f2479ecd562253e012fc8
joseph-taylor <joseph.ross.taylor@cern.ch>
2017-07-06 14:44:36 +0100

    fixed mistake with regards to readout

--- PATNTupler/interface/TimeStamp.h
+++ PATNTupler/interface/TimeStamp.h
@@ -9,16 +9,34 @@ namespace TimeStamp{
 		time_t now = time(0);
 		// tm *time = gmtime(&now); // gm time
 		tm *time = localtime(&now); // local time  
+		
 		std::string year = Form("%d", time->tm_year + 1900);
+		
 		std::string mon;
 		int mon_num = time->tm_mon + 1;
 		if (mon_num < 10) mon = Form("0%d", mon_num);
+		else mon = Form("%d", mon_num);
+
 		std::string mday;
 		int mday_num = time->tm_mday;
 		if (mday_num < 10) mday = Form("0%d", mday_num);
-		std::string hour = Form("%d", time->tm_hour);
-		std::string min = Form("%d", time->tm_min);
-		std::string sec = Form("%d", time->tm_sec);
+		else mday = Form("%d", mday_num);
+		
+		std::string hour;
+		int hour_num = time->tm_hour;
+		if (hour_num < 10) hour = Form("0%d", hour_num);
+		else hour = Form("%d", hour_num);
+
+		std::string min;
+		int min_num = time->tm_min;
+		if (min_num < 10) min = Form("0%d", min_num);
+		else min = Form("%d", min_num);
+
+		std::string sec;
+		int sec_num = time->tm_sec;
+		if (sec_num < 10) sec = Form("0%d", sec_num);
+		else sec = Form("%d", sec_num);
+
 		std::string timeStamp = year + "_" + mon + "_" + mday + "_" + hour + "_" + min + "_" + sec;
 	    return timeStamp;
 	}
