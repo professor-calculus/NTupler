b703d5230186730c47c74186ad8b2ec372babf1d
joseph-taylor <joseph.ross.taylor@cern.ch>
2017-07-18 15:19:38 +0100

    added a non WP value to use in control regions

--- PATNTupler/interface/DoubleBTagWPs.h
+++ PATNTupler/interface/DoubleBTagWPs.h
@@ -9,9 +9,11 @@ namespace DoubleBTagWPs{
 	    double dbtDouble;
 	    if (dtbString == "Off") dbtDouble = -100.0;
 	    else if (dtbString == "Loose") dbtDouble = 0.3;
-	    else if (dtbString == "Med") dbtDouble = 0.6;
+	    else if (dtbString == "Med1") dbtDouble = 0.6;
+	    else if (dtbString == "Med2") dbtDouble = 0.8;
 	    else if (dtbString == "Tight") dbtDouble = 0.9;
 	    else if (dtbString == "Max") dbtDouble = 100.0;
+	    else if (dtbString == "IDBTCv1") dbtDouble = -0.35;
 	    else{
 	        std::cout << "Have not specified the DBT string correctly\nExiting..." << std::endl;
 	        exit(EXIT_FAILURE);
