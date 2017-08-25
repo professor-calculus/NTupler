9f1323120480f897a41bc3ef5a316106d072be25
joseph-taylor <joseph.ross.taylor@cern.ch>
2017-08-25 11:26:18 +0100

    new control region dbt working points

--- PATNTupler/interface/DoubleBTagWPs.h
+++ PATNTupler/interface/DoubleBTagWPs.h
@@ -4,16 +4,19 @@
 
 namespace DoubleBTagWPs{
 
-	double dbtNameToDouble(const std::string& dtbString){
+	double dbtNameToDouble(const std::string& dbtString){
 
 	    double dbtDouble;
-	    if (dtbString == "Off") dbtDouble = -100.0;
-	    else if (dtbString == "Loose") dbtDouble = 0.3;
-	    else if (dtbString == "Med1") dbtDouble = 0.6;
-	    else if (dtbString == "Med2") dbtDouble = 0.8;
-	    else if (dtbString == "Tight") dbtDouble = 0.9;
-	    else if (dtbString == "Max") dbtDouble = 100.0;
-	    else if (dtbString == "IDBTCv1") dbtDouble = -0.35;
+	    if (dbtString == "Off") dbtDouble = -100.0;
+	    else if (dbtString == "Loose") dbtDouble = 0.3;
+	    else if (dbtString == "Med1") dbtDouble = 0.6;
+	    else if (dbtString == "Med2") dbtDouble = 0.8;
+	    else if (dbtString == "Tight") dbtDouble = 0.9;
+	    else if (dbtString == "Max") dbtDouble = 100.0;
+	    // else if (dbtString == "IDBTCv1") dbtDouble = -0.35;
+	    // else if (dbtString == "IDBTCv2") dbtDouble = -0.60;
+		else if (dbtString == "IDBTCv10") dbtDouble = 0.40;
+		else if (dbtString == "IDBTCv11") dbtDouble = -0.80;
 	    else{
 	        std::cout << "Have not specified the DBT string correctly\nExiting..." << std::endl;
 	        exit(EXIT_FAILURE);
