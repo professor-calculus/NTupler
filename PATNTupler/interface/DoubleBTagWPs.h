e23835efb42525d5ac84280d9747a95215381089
joseph-taylor <joseph.ross.taylor@cern.ch>
2017-09-19 14:36:39 +0100

    new DBT WP

--- PATNTupler/interface/DoubleBTagWPs.h
+++ PATNTupler/interface/DoubleBTagWPs.h
@@ -15,8 +15,10 @@ namespace DoubleBTagWPs{
 	    else if (dbtString == "Max") dbtDouble = 100.0;
 	    // else if (dbtString == "IDBTCv1") dbtDouble = -0.35;
 	    // else if (dbtString == "IDBTCv2") dbtDouble = -0.60;
-		else if (dbtString == "IDBTCv10") dbtDouble = 0.40;
-		else if (dbtString == "IDBTCv11") dbtDouble = -0.80;
+		// else if (dbtString == "IDBTCv10") dbtDouble = 0.40;
+		// else if (dbtString == "IDBTCv11") dbtDouble = -0.80;
+		// else if (dbtString == "IDBTCv20") dbtDouble = -0.50;
+		else if (dbtString == "IDBTCv21") dbtDouble = -0.60;
 	    else{
 	        std::cout << "Have not specified the DBT string correctly\nExiting..." << std::endl;
 	        exit(EXIT_FAILURE);
