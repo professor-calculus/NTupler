69118dee2487414fa589edb218c785d7a7d43276
joseph-taylor <joseph.ross.taylor@cern.ch>
2017-09-25 12:45:59 +0100

    updated to what was used to make ntuples

--- PATNTupler/plugins/crabConfig_nTupleProd_data_VJ.py
+++ PATNTupler/plugins/crabConfig_nTupleProd_data_VJ.py
@@ -6,7 +6,7 @@ config = config()
 # EDIT the following:
 # uniqueName, config.Data.lumiMask, config.Data.inputDataset, config.Data.unitsPerJob
 
-uniqueName = 'TESTING_JetHt_Run2016H-03Feb2017_ver3-v1_CMSSW8029wDBTV4_7b9754e' # name for this ntuple production (include commit hash so it is unique and can be traced) 
+uniqueName = 'SingleMuon_ALLEVENTSv3_Run2016H-03Feb2017_ver2-v1_CMSSW8029wDBTV4_d082ebad' # name for this ntuple production (include commit hash so it is unique and can be traced) 
 config.Data.lumiMask = 'Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt'
 
 config.General.requestName = uniqueName # name of the crab job project (eg on dashboard)
@@ -20,7 +20,7 @@ config.JobType.psetName = 'nTupleProduction_Data.py'
 # DATASETS --------------------------------------------------------------------------------------
 
 # JetHT 2016 - 03Feb2017: ~ 100k events/file (use five units per job) 
-# config.Data.inputDataset = '/JetHT/Run2016B-03Feb2017_ver1-v1/MINIAOD'
+# config.Data.inputDataset = '/JetHT/Run2016B-03Feb2017_ver1-v1/MINIAOD' # might not be any runs here in that pass masking
 # config.Data.inputDataset = '/JetHT/Run2016B-03Feb2017_ver2-v2/MINIAOD'
 # config.Data.inputDataset = '/JetHT/Run2016C-03Feb2017-v1/MINIAOD'
 # config.Data.inputDataset = '/JetHT/Run2016D-03Feb2017-v1/MINIAOD'
@@ -28,10 +28,21 @@ config.JobType.psetName = 'nTupleProduction_Data.py'
 # config.Data.inputDataset = '/JetHT/Run2016F-03Feb2017-v1/MINIAOD'
 # config.Data.inputDataset = '/JetHT/Run2016G-03Feb2017-v1/MINIAOD'
 # config.Data.inputDataset = '/JetHT/Run2016H-03Feb2017_ver2-v1/MINIAOD'
-config.Data.inputDataset = '/JetHT/Run2016H-03Feb2017_ver3-v1/MINIAOD'
+# config.Data.inputDataset = '/JetHT/Run2016H-03Feb2017_ver3-v1/MINIAOD'
+
+# SingleMuon 2016 - 03Feb2017: ~ 150k events/file (SETUP TO SELECT ALL EVENTS IN SOURCE CODE, use one unit per job)
+# config.Data.inputDataset = '/SingleMuon/Run2016B-03Feb2017_ver1-v1/MINIAOD' # might not be any runs here in that pass masking
+# config.Data.inputDataset = '/SingleMuon/Run2016B-03Feb2017_ver2-v2/MINIAOD'
+# config.Data.inputDataset = '/SingleMuon/Run2016C-03Feb2017-v1/MINIAOD'
+# config.Data.inputDataset = '/SingleMuon/Run2016D-03Feb2017-v1/MINIAOD'
+# config.Data.inputDataset = '/SingleMuon/Run2016E-03Feb2017-v1/MINIAOD'
+# config.Data.inputDataset = '/SingleMuon/Run2016F-03Feb2017-v1/MINIAOD'
+# config.Data.inputDataset = '/SingleMuon/Run2016G-03Feb2017-v1/MINIAOD'
+config.Data.inputDataset = '/SingleMuon/Run2016H-03Feb2017_ver2-v1/MINIAOD'
+# config.Data.inputDataset = '/SingleMuon/Run2016H-03Feb2017_ver3-v1/MINIAOD'
 
 #################################################################################################
-config.Data.unitsPerJob = 5
+config.Data.unitsPerJob = 1
 #################################################################################################
 
 config.Data.inputDBS = 'global' # for centrally produced data
