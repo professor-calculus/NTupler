90b000a535a119f943003c0b2ba82e2c23ca271c
joseph-taylor <joseph.ross.taylor@cern.ch>
2018-01-26 10:27:01 +0000

    does the new abcd prediction using ai and its errors

--- PATNTupler/macros/histos_plot.cc
+++ PATNTupler/macros/histos_plot.cc
@@ -20,6 +20,7 @@
 #include "MassRegionCuts.hh"
 #include "DoubleBTagWPs.h"
 #include "TimeStamp.h"
+#include "QcdSidebandCorr.h"
 
 // MAKES PLOTS USING HISTOGRAMS
 
@@ -32,7 +33,7 @@ int main(){
 
 
     // ONE: save info & luminosity
-    std::string outputDir = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/plots_2017_11_03/monteCarloDecoupled/TTJets_withAK4_subtract/"; // where we are going to save the output plots (should include the samples name, and any important features)
+    std::string outputDir = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/plots_2018_01_08/oneDimensionRepresentation/DATA/control/IDBTCv23_minus0p4/newPredictionMethod/"; // where we are going to save the output plots (should include the samples name, and any important features)
     double luminosity = 35.867; // NB this is just a label for the plot. It should match the lumi of the histograms!
     
     std::string dirExistCommand = "test -e " + outputDir;
@@ -50,27 +51,40 @@ int main(){
     // 3. sample name on the end
 
 
+    // FOR A_{i} PLOTS
+    // h_["S_anti_data"]->Divide(h_["UnD_anti_data"]);
+    // h_["S_anti_data"]->GetYaxis()->SetTitle("A_{i}");
+    // h_["S_anti_data"]->GetYaxis()->SetRangeUser(0,2.2);
+    // h_["S_anti_QCD"]->Divide(h_["UnD_anti_QCD"]);
+    // h_["S_anti_QCD"]->GetYaxis()->SetTitle("A_{i}");
+    // h_["S_anti_QCD"]->GetYaxis()->SetRangeUser(0,2.2);
+    // TH1D * h = new TH1D("h", "", 30, 0, 30);
+    // for (unsigned int i = 1; i < 31; ++i){
+    //     h->SetBinContent(i, QcdSidebandCorr::GetCorr(i));
+    //     h->SetBinError(i, 0.10);
+    //     std::cout << Form("%.10f", h_["S_anti_data"]->GetBinError(i) ) << std::endl;
+    // }
+
 
     // TWO: make plot aesthetics and saving
-    // std::vector<TH1D*> indiHistoVec = {h_["S_control_Data_JetHt2016_goldenJson"], h_["pred_control_Data_JetHt2016_goldenJson"]};
-    // std::vector<TH1D*> indiHistoVec = {h_["S_tag_TTJets"], h_["UnD_tag_TTJets"]};
-    h_["UnD_tag_TTJets"]->Scale(-1.0);
-    h_["S_tag_TTJets"]->Add(h_["UnD_tag_TTJets"]);
-    std::vector<TH1D*> indiHistoVec = {h_["S_tag_TTJets"]};
+    // std::vector<TH1D*> indiHistoVec = {h_["S_anti_QCD"], h};
+    std::vector<TH1D*> indiHistoVec = {h_["S_control_data"], h_["predNew_control_data"]};
     // std::vector<TH1D*> indiHistoVec = {h_["S_tag_mH70_mSusy2000"], h_["UnD_tag_mH70_mSusy2000"]};
-    // std::vector<TH1D*> stackHistoVec = {h_["UnD_control_WJets_NOAK4"], h_["UnD_control_ZJets_NOAK4"], h_["UnD_control_TTJets_NOAK4"], h_["UnD_control_QCD_NOAK4"]};
+    // std::vector<TH1D*> stackHistoVec = {h_["UnD_control_WJets"], h_["UnD_control_ZJets"], h_["UnD_control_TTJets"], h_["UnD_control_QCD"]};
 
     Plotter plot = Plotter(indiHistoVec);
     // Plotter plot = Plotter({}, stackHistoVec);
     // Plotter plot = Plotter(indiHistoVec, stackHistoVec);
 
-    std::vector<std::string> legendNames = {"S_tag - UnD_tag"};
-    // std::vector<std::string> legendNames = {"S tag", "U+D tag"};
-    // std::vector<std::string> legendNames = {"tag", "tag prediction"};
+    // std::vector<std::string> legendNames = {"QCD MC", "calculation"};
+    // std::vector<std::string> legendNames = {"S_anti", "UnD_anti"};
+    std::vector<std::string> legendNames = {"control", "control prediction"};
     // std::vector<std::string> legendNames = {"mH70_mSusy1200", "mH70_mSusy1600", "mH70_mSusy2000", "WJets", "ZJets", "TTJets", "QCD"};
     // std::vector<std::string> legendNames = {"WJets", "ZJets", "TTJets", "QCD"};
 
     // plot.AddLegend(legendNames, 0.70, 0.88, 0.64, 0.83, 0.028);
+    // plot.AddLegend(legendNames, 0.18, 0.38, 0.58, 0.81, 0.028);
+    // plot.AddLegend(legendNames, 0.18, 0.38, 0.18, 0.30, 0.040);
     plot.AddLegend(legendNames, 0.68, 0.88, 0.58, 0.81, 0.040);
     // plot.AddLegend(legendNames, 0.18, 0.38, 0.58, 0.81, 0.040);
     // plot.AddLegend(legendNames, 0.75, 0.88, 0.64, 0.83, 0.028);
@@ -78,12 +92,15 @@ int main(){
     // plot.AddLegend2Cols(3, legendNames, 0.70, 0.88, 0.64, 0.83, 0.028);
     // plot.AddLegend2Cols(4, legendNames, 0.70, 0.88, 0.64, 0.83, 0.028);
     
-    plot.AddLatex(luminosity);
-    // plot.AddLatex(luminosity, "#it{Data} 2016");
+    // plot.AddLatex();
+    // plot.AddLatex(luminosity);
+    plot.AddLatex(luminosity, "#it{Data} 2016");
     // plot.AddRatioBox(0.1, 1.9, "ratio", true);
     // plot.AddRatioBox("ratio", true);
     // plot.AddRatioBox("true / pred", true);
-    // plot.AddRatioBox(0.1,1.9, "true / pred", true);
+    plot.AddRatioBox(0.1,2.2, "true / pred", true);
+    // plot.AddRatioBox(0.1,2.2, "S / UnD", true);
+    // plot.AddRatioBox("S / UnD", true);
     plot.SetErrors();
     // plot.SetErrors("only_stack");
     // plot.SetErrors("only_indi");
@@ -94,9 +111,11 @@ int main(){
     
     plotName = "log";
     plot.SetLogY();
-    plot.SetYValueMin(0.15); // REMEMBER THIS PARAM! (only for log)
-    // plot.SetYValueMin(0.50); // REMEMBER THIS PARAM! (only for log)
+    // plot.SetYValueMin(0.15); // REMEMBER THIS PARAM! (only for log)
+    plot.SetYValueMin(0.80); // REMEMBER THIS PARAM! (only for log)
     // plot.SetYValueMin(1.10); // REMEMBER THIS PARAM! (only for log)
+    // plot.SetYValueMin(3.10); // REMEMBER THIS PARAM! (only for log)
+    // plot.SetYValueMin(12.10); // REMEMBER THIS PARAM! (only for log)
     plot.SaveSpec01(Form("%s/%s.pdf", outputDir.c_str(), plotName.c_str()), stringVec);
 
 
@@ -111,43 +130,43 @@ int main(){
 void GetHistograms(std::map<std::string,TH1D*>& h_)
 {
     // histos locations
-    std::string preamble = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/histos_2017_09_28_CMSSW_8_0_29_dbtV4/MassCutsV04/";
+    std::string preamble = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/histos_2018_01_11_CMSSW_8_0_29_dbtV4/MassCutsV05/";
     
-    std::string postamble = "MassCutsV04_ak8pt300_ht1500x2500x3500x_ak4pt250n250_lumi37.root";
+    std::string postamble = "MassCutsV05_ak8pt300_ht1500x2500x3500x_ak4pt300n-1_lumi36.root";
     std::vector<std::string> histoNameVec;
-    // histoNameVec.push_back("Data_JetHt2016_goldenJson"); // comment out when working on MC
+    histoNameVec.push_back("data");
     histoNameVec.push_back("QCD");
     histoNameVec.push_back("TTJets");
     histoNameVec.push_back("ZJets");
     histoNameVec.push_back("WJets");
-    histoNameVec.push_back("mH30_mSusy800");
-    histoNameVec.push_back("mH50_mSusy800");
-    histoNameVec.push_back("mH70_mSusy800");
-    histoNameVec.push_back("mH90_mSusy800");
-    histoNameVec.push_back("mH30_mSusy1200");
-    histoNameVec.push_back("mH50_mSusy1200");
-    histoNameVec.push_back("mH70_mSusy1200");
-    histoNameVec.push_back("mH90_mSusy1200");
-    histoNameVec.push_back("mH30_mSusy1600");
-    histoNameVec.push_back("mH50_mSusy1600");
-    histoNameVec.push_back("mH70_mSusy1600");
-    histoNameVec.push_back("mH90_mSusy1600");
-    histoNameVec.push_back("mH30_mSusy2000");
-    histoNameVec.push_back("mH50_mSusy2000");
-    histoNameVec.push_back("mH70_mSusy2000");
-    histoNameVec.push_back("mH90_mSusy2000");
+    // histoNameVec.push_back("mH30_mSusy800");
+    // histoNameVec.push_back("mH50_mSusy800");
+    // histoNameVec.push_back("mH70_mSusy800");
+    // histoNameVec.push_back("mH90_mSusy800");
+    // histoNameVec.push_back("mH30_mSusy1200");
+    // histoNameVec.push_back("mH50_mSusy1200");
+    // histoNameVec.push_back("mH70_mSusy1200");
+    // histoNameVec.push_back("mH90_mSusy1200");
+    // histoNameVec.push_back("mH30_mSusy1600");
+    // histoNameVec.push_back("mH50_mSusy1600");
+    // histoNameVec.push_back("mH70_mSusy1600");
+    // histoNameVec.push_back("mH90_mSusy1600");
+    // histoNameVec.push_back("mH30_mSusy2000");
+    // histoNameVec.push_back("mH50_mSusy2000");
+    // histoNameVec.push_back("mH70_mSusy2000");
+    // histoNameVec.push_back("mH90_mSusy2000");
 
-    std::string postamble_noAk4 = "MassCutsV04_ak8pt300_ht1500x2500x3500x_ak4pt-1n-1_lumi37.root"; // comment out when working on MC
+    std::string postamble_noAk4 = "MassCutsV05_ak8pt300_ht1500x2500x3500x_ak4pt-1n-1_lumi36.root";
     std::vector<std::string> histoNameVec_noAk4;
-    // histoNameVec_noAk4.push_back("Data_JetHt2016_goldenJson_NOAK4");
+    histoNameVec_noAk4.push_back("data_NOAK4");
     histoNameVec_noAk4.push_back("QCD_NOAK4");
     histoNameVec_noAk4.push_back("TTJets_NOAK4");
     histoNameVec_noAk4.push_back("ZJets_NOAK4");
     histoNameVec_noAk4.push_back("WJets_NOAK4");
-    histoNameVec_noAk4.push_back("mH70_mSusy800_NOAK4");
-    histoNameVec_noAk4.push_back("mH70_mSusy1200_NOAK4");
-    histoNameVec_noAk4.push_back("mH70_mSusy1600_NOAK4");
-    histoNameVec_noAk4.push_back("mH70_mSusy2000_NOAK4");
+    // histoNameVec_noAk4.push_back("mH70_mSusy800_NOAK4");
+    // histoNameVec_noAk4.push_back("mH70_mSusy1200_NOAK4");
+    // histoNameVec_noAk4.push_back("mH70_mSusy1600_NOAK4");
+    // histoNameVec_noAk4.push_back("mH70_mSusy2000_NOAK4");
 
     for (size_t iH = 0; iH < (histoNameVec.size() + histoNameVec_noAk4.size()); ++iH){
 
@@ -168,23 +187,23 @@ void GetHistograms(std::map<std::string,TH1D*>& h_)
         // 2. tag, anti, control --> refers to 2*DBT space
         // 3. sample name on the end
 
-        h_[Form("S_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get("S_dbtMed2MaxAndMed2Max"); // comment out when working on control data
-        h_[Form("U_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get("U_dbtMed2MaxAndMed2Max"); // comment out when working on control data
-        h_[Form("D_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get("D_dbtMed2MaxAndMed2Max"); // comment out when working on control data
+        if (histoToUse != "data" && histoToUse != "data_NOAK4") h_[Form("S_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get("S_dbtDiagUpLoose");
+        h_[Form("U_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get("U_dbtDiagUpLoose");
+        h_[Form("D_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get("D_dbtDiagUpLoose");
 
         h_[Form("S_anti_%s", histoToUse.c_str())] = (TH1D*)f->Get("S_dbtOffLooseAndOffLoose");
         h_[Form("U_anti_%s", histoToUse.c_str())] = (TH1D*)f->Get("U_dbtOffLooseAndOffLoose");
         h_[Form("D_anti_%s", histoToUse.c_str())] = (TH1D*)f->Get("D_dbtOffLooseAndOffLoose");
 
-        h_[Form("S_control_%s", histoToUse.c_str())] = (TH1D*)f->Get("S_dbtLooseMed2AndOffIDBTCv21");
-        h_[Form("U_control_%s", histoToUse.c_str())] = (TH1D*)f->Get("U_dbtLooseMed2AndOffIDBTCv21");
-        h_[Form("D_control_%s", histoToUse.c_str())] = (TH1D*)f->Get("D_dbtLooseMed2AndOffIDBTCv21");
-        h_[Form("S_control_%s", histoToUse.c_str())]->Add((TH1D*)f->Get("S_dbtOffIDBTCv21AndLooseMed2"));
-        h_[Form("U_control_%s", histoToUse.c_str())]->Add((TH1D*)f->Get("U_dbtOffIDBTCv21AndLooseMed2"));
-        h_[Form("D_control_%s", histoToUse.c_str())]->Add((TH1D*)f->Get("D_dbtOffIDBTCv21AndLooseMed2"));
+        h_[Form("S_control_%s", histoToUse.c_str())] = (TH1D*)f->Get("S_dbtLooseMed2AndOffIDBTCv23");
+        h_[Form("U_control_%s", histoToUse.c_str())] = (TH1D*)f->Get("U_dbtLooseMed2AndOffIDBTCv23");
+        h_[Form("D_control_%s", histoToUse.c_str())] = (TH1D*)f->Get("D_dbtLooseMed2AndOffIDBTCv23");
+        h_[Form("S_control_%s", histoToUse.c_str())]->Add((TH1D*)f->Get("S_dbtOffIDBTCv23AndLooseMed2"));
+        h_[Form("U_control_%s", histoToUse.c_str())]->Add((TH1D*)f->Get("U_dbtOffIDBTCv23AndLooseMed2"));
+        h_[Form("D_control_%s", histoToUse.c_str())]->Add((TH1D*)f->Get("D_dbtOffIDBTCv23AndLooseMed2"));
 
-        h_[Form("UnD_tag_%s", histoToUse.c_str())] = (TH1D*)h_[Form("U_tag_%s", histoToUse.c_str())]->Clone(); // comment out when working on control data
-        h_[Form("UnD_tag_%s", histoToUse.c_str())]->Add(h_[Form("D_tag_%s", histoToUse.c_str())]); // comment out when working on control data
+        h_[Form("UnD_tag_%s", histoToUse.c_str())] = (TH1D*)h_[Form("U_tag_%s", histoToUse.c_str())]->Clone();
+        h_[Form("UnD_tag_%s", histoToUse.c_str())]->Add(h_[Form("D_tag_%s", histoToUse.c_str())]);
 
         h_[Form("UnD_anti_%s", histoToUse.c_str())] = (TH1D*)h_[Form("U_anti_%s", histoToUse.c_str())]->Clone();
         h_[Form("UnD_anti_%s", histoToUse.c_str())]->Add(h_[Form("D_anti_%s", histoToUse.c_str())]);
@@ -192,13 +211,42 @@ void GetHistograms(std::map<std::string,TH1D*>& h_)
         h_[Form("UnD_control_%s", histoToUse.c_str())] = (TH1D*)h_[Form("U_control_%s", histoToUse.c_str())]->Clone();
         h_[Form("UnD_control_%s", histoToUse.c_str())]->Add(h_[Form("D_control_%s", histoToUse.c_str())]);
    
-        h_[Form("pred_tag_%s", histoToUse.c_str())] = (TH1D*)h_[Form("UnD_tag_%s", histoToUse.c_str())]->Clone(); // comment out when working on control data
-        h_[Form("pred_tag_%s", histoToUse.c_str())]->Multiply(h_[Form("S_anti_%s", histoToUse.c_str())]); // comment out when working on control data
-        h_[Form("pred_tag_%s", histoToUse.c_str())]->Divide(h_[Form("UnD_anti_%s", histoToUse.c_str())]); // comment out when working on control data
+        // NEW METHOD OF PREDICTION
+        h_[Form("predNew_tag_%s", histoToUse.c_str())] = (TH1D*)h_[Form("UnD_tag_%s", histoToUse.c_str())]->Clone();
+        for (int iBin = 1; iBin < h_[Form("predNew_tag_%s", histoToUse.c_str())]->GetNbinsX() + 1; ++iBin){
+            double corrValue = QcdSidebandCorr::GetCorr(iBin);
+            double corrError = QcdSidebandCorr::GetCorrErr(iBin);
+            double UnDValue = h_[Form("predNew_tag_%s", histoToUse.c_str())]->GetBinContent(iBin);
+            double UnDError = h_[Form("predNew_tag_%s", histoToUse.c_str())]->GetBinError(iBin);
+            double predValue = corrValue * UnDValue;
+            double predError = 0.0;
+            if (UnDValue != 0) predError = predValue * sqrt( (corrError/corrValue)*(corrError/corrValue) + (UnDError/UnDValue)*(UnDError/UnDValue) );
+            h_[Form("predNew_tag_%s", histoToUse.c_str())]->SetBinContent(iBin, predValue);
+            h_[Form("predNew_tag_%s", histoToUse.c_str())]->SetBinError(iBin, predError);
+        }
 
-        h_[Form("pred_control_%s", histoToUse.c_str())] = (TH1D*)h_[Form("UnD_control_%s", histoToUse.c_str())]->Clone();
-        h_[Form("pred_control_%s", histoToUse.c_str())]->Multiply(h_[Form("S_anti_%s", histoToUse.c_str())]);
-        h_[Form("pred_control_%s", histoToUse.c_str())]->Divide(h_[Form("UnD_anti_%s", histoToUse.c_str())]);
+        h_[Form("predNew_control_%s", histoToUse.c_str())] = (TH1D*)h_[Form("UnD_control_%s", histoToUse.c_str())]->Clone();
+        for (int iBin = 1; iBin < h_[Form("predNew_control_%s", histoToUse.c_str())]->GetNbinsX() + 1; ++iBin){
+            double corrValue = QcdSidebandCorr::GetCorr(iBin);
+            double corrError = QcdSidebandCorr::GetCorrErr(iBin);
+            double UnDValue = h_[Form("predNew_control_%s", histoToUse.c_str())]->GetBinContent(iBin);
+            double UnDError = h_[Form("predNew_control_%s", histoToUse.c_str())]->GetBinError(iBin);
+            double predValue = corrValue * UnDValue;
+            double predError = 0.0;
+            if (UnDValue != 0) predError = predValue * sqrt( (corrError/corrValue)*(corrError/corrValue) + (UnDError/UnDValue)*(UnDError/UnDValue) );
+            h_[Form("predNew_control_%s", histoToUse.c_str())]->SetBinContent(iBin, predValue);
+            h_[Form("predNew_control_%s", histoToUse.c_str())]->SetBinError(iBin, predError);
+        }
+
+        // OLD METHOD OF PREDICTION
+        h_[Form("predOld_tag_%s", histoToUse.c_str())] = (TH1D*)h_[Form("UnD_tag_%s", histoToUse.c_str())]->Clone();
+        h_[Form("predOld_tag_%s", histoToUse.c_str())]->Multiply(h_[Form("S_anti_%s", histoToUse.c_str())]);
+        h_[Form("predOld_tag_%s", histoToUse.c_str())]->Divide(h_[Form("UnD_anti_%s", histoToUse.c_str())]);
+
+        h_[Form("predOld_control_%s", histoToUse.c_str())] = (TH1D*)h_[Form("UnD_control_%s", histoToUse.c_str())]->Clone();
+        h_[Form("predOld_control_%s", histoToUse.c_str())]->Multiply(h_[Form("S_anti_%s", histoToUse.c_str())]);
+        h_[Form("predOld_control_%s", histoToUse.c_str())]->Divide(h_[Form("UnD_anti_%s", histoToUse.c_str())]);
+
 
     } // closes loop through histoNameVec
 }
