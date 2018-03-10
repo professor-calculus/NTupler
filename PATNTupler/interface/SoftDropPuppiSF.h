2dca83ea8f867bcf854cf6aa8b6b7bdcb23b234a
joseph-taylor <joseph.ross.taylor@cern.ch>
2018-03-10 12:12:12 +0000

    puppi soft drop info

---
+++ PATNTupler/interface/SoftDropPuppiSF.h
@@ -0,0 +1,17 @@
+#ifndef SOFTDROPPUPPISF_H
+#define SOFTDROPPUPPISF_H
+
+#include "SoftDropPuppiSF.h"
+
+namespace SoftDropPuppiSF{
+
+	double get_jetMassScaleSF() {return 1.0;}
+	double get_jetMassScaleSFUp() {return 1.0 + 0.0094;}
+	double get_jetMassScaleSFDown() {return 1.0 - 0.0094;}
+	double get_jetMassResolutionSF() {return 1.0;}
+	double get_jetMassResolutionSFUp() {return 1 + 0.20;}
+	double get_jetMassResolutionSFDown() {return 1 - 0.20;}
+	double get_mcMassResolution() {return 10.1;}
+
+} // closes namespace 'SoftDropPuppiSF'
+#endif
