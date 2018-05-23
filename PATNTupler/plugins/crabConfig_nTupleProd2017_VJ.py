792ddcdb712ff6e2ea43fe89430cd34b15e19c0f
joseph-taylor <joseph.ross.taylor@cern.ch>
2018-05-23 16:59:32 +0100

    adding complete 2017 crab submission

---
+++ PATNTupler/plugins/crabConfig_nTupleProd2017_VJ.py
@@ -0,0 +1,63 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+# submit, from plugins directory, with $ crab submit -c crabConfig_nTupleProd2017_VJ.py
+# check $ cmsRun nTupleProduction2017.py, is setup correctly first (and that RALMiniAnalyzer is compiled)!!!
+# EDIT the following:
+# uniqueName, config.Data.inputDataset, config.Data.inputDBS, config.Data.unitsPerJob
+
+uniqueName = 'QCD_HT1000to1500_CMSSW947_COMMITHASH' # name for this ntuple production (include commit hash so it is unique and can be traced) 
+
+config.General.requestName = uniqueName # name of the crab job project (eg on dashboard)
+config.General.workArea = 'crab_projects' # name of directory where crab project info is stored
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'nTupleProduction2017.py'
+config.JobType.inputFiles = ['puppiCorr.root']
+
+#################################################################################################
+# DATASETS --------------------------------------------------------------------------------------
+
+# TT to SemiLeptonic: 41M+111M(PS) events: (use three units per job)
+# config.Data.inputDataset = '/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM'
+# config.Data.inputDataset = '/TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+
+# TT to Hadronic: 42M+131M(PS) events: (use three units per job)
+# config.Data.inputDataset = '/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+# config.Data.inputDataset = '/TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+
+# TT to 2L2Nu: 9M+67M(PS) events: (use three units per job)
+# config.Data.inputDataset = '/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+# config.Data.inputDataset = '/TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+
+# Z to Jets HT800+: 9.8M events: (use three units per job) 
+# config.Data.inputDataset = '/ZJetsToQQ_HT-800toInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+
+# W to Jets HT800+: 9.4M events: (use four units per job)
+# config.Data.inputDataset = '/WJetsToQQ_HT-800toInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1/MINIAODSIM'
+
+# QCD HT1000-1500: 16.6M events: (use three units per job)
+config.Data.inputDataset = '/QCD_HT1000to1500_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+
+# QCD HT1500-2000: 11.3M events: (use three units per job)
+# config.Data.inputDataset = '/QCD_HT1500to2000_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_old_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+
+# QCD HT2000+: 5.4M events: (use three units per job)
+# config.Data.inputDataset = '/QCD_HT2000toInf_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_old_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+
+# SIGNAL SAMPLES: 200K events & 150 files (use 30 units per job)
+# config.Data.inputDataset = 'DATASETXYZ' # use the script quickSignalSubmit_crab.py to deploy
+
+#################################################################################################
+config.Data.inputDBS = 'global' # for centrally produced data
+# config.Data.inputDBS = 'phys03' # for personal simulation
+config.Data.unitsPerJob = 3
+#################################################################################################
+
+config.Data.totalUnits = -1 # '-1' does them all
+config.Data.splitting = 'FileBased'
+config.Data.publication = False
+config.Data.outLFNDirBase = '/store/user/taylor/ntuples_MC17_May2018' # Wherever you want the ntuples to go on the RAL T2
+
+config.Site.storageSite = 'T2_UK_SGrid_RALPP'
---
+++ PATNTupler/plugins/crabConfig_nTupleProd_data2017_VJ.py
@@ -0,0 +1,48 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+# submit, from plugins directory, with $ crab submit -c crabConfig_nTupleProd_data2017_VJ.py
+# check $ cmsRun nTupleProduction_Data2017.py, is setup correctly first (and that RALMiniAnalyzer is compiled)!!!
+# EDIT the following:
+# uniqueName, config.Data.lumiMask, config.Data.inputDataset, config.Data.unitsPerJob
+
+uniqueName = 'JetHt_Run2017B_CMSSW947_COMMITHASH' # name for this ntuple production (include commit hash so it is unique and can be traced) 
+
+config.Data.lumiMask = 'Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt'
+
+config.General.requestName = uniqueName # name of the crab job project (eg on dashboard)
+config.General.workArea = 'crab_projects' # name of directory where crab project info is stored
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'nTupleProduction_Data2017.py'
+config.JobType.inputFiles = ['puppiCorr.root']
+
+#################################################################################################
+# DATASETS --------------------------------------------------------------------------------------
+
+# JetHT 2017 - 31Mar2018: (use five units per job) 
+config.Data.inputDataset = '/JetHT/Run2017B-31Mar2018-v1/MINIAOD'
+# config.Data.inputDataset = '/JetHT/Run2017C-31Mar2018-v1/MINIAOD'
+# config.Data.inputDataset = '/JetHT/Run2017D-31Mar2018-v1/MINIAOD'
+# config.Data.inputDataset = '/JetHT/Run2017E-31Mar2018-v1/MINIAOD'
+# config.Data.inputDataset = '/JetHT/Run2017F-31Mar2018-v1/MINIAOD'
+
+# SingleMuon 2017 - 31Mar2018: (SETUP TO SELECT ALL EVENTS IN SOURCE CODE, use one unit per job)
+# config.Data.inputDataset = '/SingleMuon/Run2017B-31Mar2018-v1/MINIAOD'
+# config.Data.inputDataset = '/SingleMuon/Run2017C-31Mar2018-v1/MINIAOD'
+# config.Data.inputDataset = '/SingleMuon/Run2017D-31Mar2018-v1/MINIAOD'
+# config.Data.inputDataset = '/SingleMuon/Run2017E-31Mar2018-v1/MINIAOD'
+# config.Data.inputDataset = '/SingleMuon/Run2017F-31Mar2018-v1/MINIAOD'
+
+#################################################################################################
+config.Data.unitsPerJob = 5
+#################################################################################################
+
+config.Data.inputDBS = 'global' # for centrally produced data
+config.Data.totalUnits = -1 # '-1' does them all
+config.Data.splitting = 'FileBased'
+config.Data.publication = False
+config.Data.outLFNDirBase = '/store/user/taylor/ntuples_DATA17_May2018' # Wherever you want the ntuples to go on the RAL T2
+
+config.Site.storageSite = 'T2_UK_SGrid_RALPP'
