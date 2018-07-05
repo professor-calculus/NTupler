03834472ef872a9ef03b0700723ecc951da626ca
Alexander Titterton <alextitterton@physics.org>
2018-07-05 16:25:47 +0100

    Added nFatJets and removed fat jets requirement

--- PATNTupler/main/mainNMSSM.cc
+++ PATNTupler/main/mainNMSSM.cc
@@ -1150,8 +1150,8 @@ int main(int argc, char** argv){
 				else doubleBFatJetPairTree.fillTree(sampleType, *evtInfo, fatJetB, fatJetA, ht, ht_jecUncUp, ht_jecUncDown, ht_jerUncUp, ht_jerUncDown, mht, mht_jecUncUp, mht_jecUncDown, mht_jerUncUp, mht_jerUncDown, slimJets, allSlimJets, slimBJets, allSlimBJets, doesEventPassTrigger, nPU, nISR, nGluino, D_factor);
 			}
 			else {
-				const ran::NtFatJet& fatJetA = NULL;
-				const ran::NtFatJet& fatJetB = NULL;
+				const ran::NtFatJet& fatJetA;
+				const ran::NtFatJet& fatJetB;
 
 				std::vector<ran::NtJet> slimJets;
 				std::vector<ran::NtJet> slimBJets;
