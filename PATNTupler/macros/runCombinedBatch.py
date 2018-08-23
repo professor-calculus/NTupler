481f62e7e6219db0a46924a89120823845adbf8d
joseph-taylor <joseph.ross.taylor@cern.ch>
2018-08-23 13:33:17 +0100

    added new management of condor files and logs

--- PATNTupler/macros/runCombinedBatch.py
+++ PATNTupler/macros/runCombinedBatch.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import re
+import time
 
 # run with
 # $ python $CMSSW_BASE/src/NTupler/PATNTupler/macros/runCombinedBatch.py
@@ -12,8 +13,20 @@ import re
 #############################
 ### ## # USER INPUTS # ## ###
 
-# the combined process you desire
-combinedMethod = "AsymptoticLimits"
+# A COMMA SEPARATED LIST OF NUISANCES THAT YOU WISH TO FREEZE.
+
+nuisancesToFreeze = "" # >>> no freezing a thing
+# nuisancesToFreeze = "SigDbtTag2016,SigDbtTag2017"
+# nuisancesToFreeze = "TtDbtTag2016,TtDbtTag2017"
+# nuisancesToFreeze = "XS_TTJets2016,XS_TTJets0L2017,XS_TTJets1L2017,XS_TTJets2L2017"
+# nuisancesToFreeze = "luminosity2016,luminosity2017"
+# nuisancesToFreeze = "jecAK4Unc2016,jecAK4Unc2017"
+# nuisancesToFreeze = "jerAK4Unc2016,jerAK4Unc2017"
+# nuisancesToFreeze = "jecAK8Unc2016,jecAK8Unc2017"
+# nuisancesToFreeze = "jerAK8Unc2016,jerAK8Unc2017"
+# nuisancesToFreeze = "jmsUnc2016,jmsUnc2017"
+# nuisancesToFreeze = "jmrUnc2016,jmrUnc2017"
+# nuisancesToFreeze = "isr2016,isr2017"
 
 #############################
 #############################
@@ -26,6 +39,11 @@ cmsswBase = cmsswBase.rstrip()
 inputDir = os.getcwd()
 
 signalDirs = os.popen("ls %s" % inputDir, "r").readlines()
+
+timeStamp = str(time.localtime().tm_year) + "_" + str(time.localtime().tm_mon) + "_" + str(time.localtime().tm_mday) + "_"
+timeStamp += str(time.localtime().tm_hour) + "_" + str(time.localtime().tm_min) + "_" + str(time.localtime().tm_sec)
+batchDir = "batchFiles_" + timeStamp
+os.mkdir(batchDir)
 
 for signalDir in signalDirs:
 
@@ -74,14 +92,14 @@ for signalDir in signalDirs:
 
     condorFileName = signalDir + ".condor"
     jobFileName = signalDir + ".sh"
-    f = open("%s" % condorFileName, 'w')
+
+    f = open("%s/%s" % (batchDir,condorFileName), 'w')
     f.write("Universe                = vanilla\n")
-    f.write("Executable              = %s\n" % jobFileName)
-    f.write("Log                     = /opt/ppd/scratch/xap79297/jobLogs/combined/%s.log\n" % signalDir)
-    f.write("Output                  = /opt/ppd/scratch/xap79297/jobLogs/combined/%s.out\n" % signalDir)
-    f.write("Error                   = /opt/ppd/scratch/xap79297/jobLogs/combined/%s.err\n" % signalDir)
+    f.write("Executable              = %s/%s\n" % (batchDir,jobFileName) )
+    f.write("Log                     = /opt/ppd/scratch/xap79297/jobLogs/combined/%s__%s.log\n" % (timeStamp,signalDir) )
+    f.write("Output                  = /opt/ppd/scratch/xap79297/jobLogs/combined/%s__%s.out\n" % (timeStamp,signalDir) )
+    f.write("Error                   = /opt/ppd/scratch/xap79297/jobLogs/combined/%s__%s.err\n" % (timeStamp,signalDir) )
     f.write("Request_memory          = 1 GB\n")
-    f.write("Transfer_input_files    = %s\n" % jobFileName)
     f.write("should_transfer_files   = YES\n")
     f.write("when_to_transfer_output = ON_EXIT_OR_EVICT\n")
     f.write("#\n")
@@ -89,16 +107,19 @@ for signalDir in signalDirs:
     f.write("Queue\n")
     f.close()
 
-    g = open("%s" % jobFileName, 'w')
+    g = open("%s/%s" % (batchDir,jobFileName), 'w')
     g.write("#!/bin/bash\n")
     g.write("cd %s\n" % cmsswBase)
     g.write("eval `scramv1 runtime -sh`\n")
     g.write("cd %s\n" % inputDir)
-    g.write("combine -M %s --mass %s --keyword-value mSusy=%s %s\n" % (combinedMethod, higgsMass, susyMass, fileToUse) )
+    if (len(nuisancesToFreeze) == 0):
+        g.write("combine -M AsymptoticLimits --mass %s --keyword-value mSusy=%s %s\n" % (higgsMass, susyMass, fileToUse) )
+    else:
+        g.write("combine -M AsymptoticLimits --freezeParameter %s --mass %s --keyword-value mSusy=%s %s\n" % (nuisancesToFreeze, higgsMass, susyMass, fileToUse) )
     g.close()
-    os.chmod(jobFileName, 0755)
+    os.chmod("%s/%s" % (batchDir,jobFileName), 0755)
 
-    os.system("condor_submit %s" % condorFileName)
-    # print "condor_submit %s" % condorFileName
+    # print "condor_submit %s/%s" % (batchDir,condorFileName)
+    os.system("condor_submit %s/%s" % (batchDir,condorFileName) )
     print ""
     print ""
