85afcd13589e6f07402ee8541693e14d8083c34f
Alexander Titterton <alextitterton@physics.org>
2018-10-04 12:52:28 +0100

    Added TrackStruct and PhotonStruct source files

---
+++ PATNTupler/src/PhotonStruct.cc
@@ -0,0 +1,12 @@
+#include "../interface/PhotonStruct.hh"
+
+ClassImp(ran::PhotonStruct)
+
+/// RAL Analysis Ntuples library namespace
+namespace ran{
+
+    PhotonStruct::PhotonStruct() : pt(0), eta(0){
+
+  }
+
+}
