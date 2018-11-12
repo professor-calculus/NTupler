9b789f29ed1a427cbf3177ac996040763583988c
joseph-taylor <joseph.ross.taylor@cern.ch>
2018-11-12 15:50:01 +0000

    added 15 percent qcd error

--- PATNTupler/macros/histos_plot_special.cc
+++ PATNTupler/macros/histos_plot_special.cc
@@ -32,7 +32,7 @@ int main(){
 
 
     // ONE: save info & luminosity
-    std::string outputDir = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/plots_2018_08_03/2016_80X/testingTTJets/threeBinz/dbtNom/stacks/S_anti__withData/"; // where we are going to save the output plots (should include the samples name, and any important features)
+    std::string outputDir = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/plots_2018_08_03/2016_80X/testingTTJets_v2/threeBinz/dbtNom/theTest_wQcdErr15/"; // where we are going to save the output plots (should include the samples name, and any important features)
     double luminosity = 35.867; // NB this is just a label for the plot. It should match the lumi of the histograms!
     // double luminosity = 41.370; // 2017 Plots::: NB this is just a label for the plot. It should match the lumi of the histograms!
     
@@ -50,7 +50,26 @@ int main(){
     // 3. sample name on the end
 
 
-    // HISTO MANIPULATION
+
+    // TWO: scale TTJets XS
+    const double scaleTTJetsXS = 1.0;
+
+    h_["S_anti_TTJets"]->Scale(scaleTTJetsXS);
+    h_["S_tag_TTJets"]->Scale(scaleTTJetsXS);
+    h_["UnD_anti_TTJets"]->Scale(scaleTTJetsXS);
+    h_["UnD_tag_TTJets"]->Scale(scaleTTJetsXS);
+
+
+
+    // QCD MC HISTO MANIPULATION
+    TH1D * h_qcdF = (TH1D*)h_["S_anti_QCD"]->Clone();
+    h_qcdF->Divide(h_["UnD_anti_QCD"]);
+    TH1D * h_qcdEst = (TH1D*)h_["UnD_tag_QCD"]->Clone();
+    h_qcdEst->Multiply(h_qcdF);
+    TH1D * h_qcdCorr = (TH1D*)h_["S_tag_QCD"]->Clone();
+    h_qcdCorr->Divide(h_qcdEst);
+
+    // ~DATA HISTO MANIPULATION
     TH1D * hS = (TH1D*)h_["S_anti_data"]->Clone();
     hS->Add(h_["S_anti_TTJets"], -1);
     hS->Add(h_["S_anti_ZJets"], -1);
@@ -65,15 +84,20 @@ int main(){
     hF_withMC->Divide(hUnD);
     hF_withMC->GetYaxis()->SetTitle("F_{i}");
 
-    // TH1D * hF_noMC = (TH1D*)h_["S_anti_data"]->Clone();
-    // hF_noMC->Divide(h_["UnD_anti_data"]);
-    // hF_noMC->GetYaxis()->SetTitle("F_{i}");
-
     TH1D * h_ddQCD = (TH1D*)h_["UnD_tag_data"]->Clone();
     h_ddQCD->Add(h_["UnD_tag_TTJets"], -1);
     h_ddQCD->Add(h_["UnD_tag_WJets"], -1);
     h_ddQCD->Add(h_["UnD_tag_ZJets"], -1);
     h_ddQCD->Multiply(hF_withMC);
+    TH1D * h_err = new TH1D("h_err", "", 3, 0, 3);
+    // 15 percent error on QCD estimation
+    h_err->SetBinContent(1,1.0);
+    h_err->SetBinContent(2,1.0);
+    h_err->SetBinContent(3,1.0);
+    h_err->SetBinError(1,0.15);
+    h_err->SetBinError(2,0.15);
+    h_err->SetBinError(3,0.15);
+    h_ddQCD->Multiply(h_err);
 
     TH1D * h_ddTT = (TH1D*)h_["S_tag_data"]->Clone();
     h_ddTT->Add(h_ddQCD, -1);
@@ -81,26 +105,30 @@ int main(){
     h_ddTT->Add(h_["S_tag_WJets"], -1);
 
 
-    // TWO: make plot aesthetics and saving
-    // std::vector<TH1D*> indiHistoVec = {h_ddTT, h_["S_tag_TTJets"]};
-    // std::vector<TH1D*> indiHistoVec = {h_ddQCD};
-    std::vector<TH1D*> indiHistoVec = {h_["S_anti_data"]};
-    std::vector<TH1D*> stackHistoVec = {h_["S_anti_WJets"], h_["S_anti_ZJets"], h_["S_anti_TTJets"], h_["S_anti_QCD"]};
 
-    // Plotter plot = Plotter(indiHistoVec);
+    // THREE: make plot aesthetics and saving
+    // std::vector<TH1D*> indiHistoVec = {h_qcdCorr};
+    std::vector<TH1D*> indiHistoVec = {h_ddTT, h_["S_tag_TTJets"]};
+    // std::vector<TH1D*> indiHistoVec = {hF_withMC, h_qcdF};
+    // std::vector<TH1D*> indiHistoVec = {h_["S_tag_QCD"], h_qcdEst};
+    // std::vector<TH1D*> stackHistoVec = {h_["S_anti_WJets"], h_["S_anti_ZJets"], h_["S_anti_TTJets"], h_["S_anti_QCD"]};
+
+    Plotter plot = Plotter(indiHistoVec);
     // Plotter plot = Plotter({}, stackHistoVec);
-    Plotter plot = Plotter(indiHistoVec, stackHistoVec);
+    // Plotter plot = Plotter(indiHistoVec, stackHistoVec);
 
-    // std::vector<std::string> legendNames = {"data driven", "MC"};
-    std::vector<std::string> legendNames = {"data", "WJets", "ZJets", "TTJets", "QCD"};
+    // std::vector<std::string> legendNames = {"S_tag", "S_tag prediction"};
+    std::vector<std::string> legendNames = {"data driven", "MC"};
+    // std::vector<std::string> legendNames = {"data", "WJets", "ZJets", "TTJets", "QCD"};
 
     plot.AddLegend(legendNames, 0.70, 0.88, 0.68, 0.86, 0.045);
     // plot.AddLegend(legendNames, 0.70, 0.88, 0.63, 0.86, 0.045);
     
-    plot.AddLatex(luminosity);
-    // plot.AddLatex(luminosity, "#it{Preliminary}");
+    // plot.AddLatex(luminosity);
+    plot.AddLatex(luminosity, "#it{Preliminary}");
     
-    // plot.AddRatioBox(0.10, 1.90, "data / MC", true);
+    plot.AddRatioBox(0.10, 1.90, "data / MC", true);
+    // plot.AddRatioBox(0.10, 1.90, "true / pred", true);
     
     plot.SetErrors();
 
@@ -122,8 +150,8 @@ void GetHistograms(std::map<std::string,TH1D*>& h_)
 
     std::string ttbarDbtSysToUse = "NOSYS";
     
-    std::string preamble = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/histos_2018_08_03/MassCutsSpecialV06/run2016/";  
-    // std::string preamble = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/histos_2018_08_03/MassCutsSpecialV06/run2017/";  
+    std::string preamble = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/histos_2018_08_03/MassCutsSpecialV06/run2016_v2/";  
+    // std::string preamble = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/histos_2018_08_03/MassCutsSpecialV06/run2017_v2/";  
     
     std::string postamble = "MassCutsSpecialV06_ak8pt300_ht1500x_ak4pt-1n-1_lumi36.root";
     // std::string postamble = "MassCutsSpecialV06_ak8pt300_ht1500x_ak4pt-1n-1_lumi41.root";
@@ -151,9 +179,9 @@ void GetHistograms(std::map<std::string,TH1D*>& h_)
         std::string systematicAppend = "NOSYS";
         if (histoToUse == "TTJets") systematicAppend = ttbarDbtSysToUse;
 
-        h_[Form("S_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get( Form("S_dbtDiagUpLoose_%s", systematicAppend.c_str() ) );
-        h_[Form("U_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get( Form("U_dbtDiagUpLoose_%s", systematicAppend.c_str() ) );
-        h_[Form("D_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get( Form("D_dbtDiagUpLoose_%s", systematicAppend.c_str() ) );
+        h_[Form("S_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get( Form("S_dbtLooseMaxAndLooseMax_%s", systematicAppend.c_str() ) );
+        h_[Form("U_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get( Form("U_dbtLooseMaxAndLooseMax_%s", systematicAppend.c_str() ) );
+        h_[Form("D_tag_%s", histoToUse.c_str())] = (TH1D*)f->Get( Form("D_dbtLooseMaxAndLooseMax_%s", systematicAppend.c_str() ) );
         
         h_[Form("S_anti_%s", histoToUse.c_str())] = (TH1D*)f->Get("S_dbtOffLooseAndOffLoose_NOSYS");
         h_[Form("U_anti_%s", histoToUse.c_str())] = (TH1D*)f->Get("U_dbtOffLooseAndOffLoose_NOSYS");
